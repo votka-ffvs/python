de94be0709cac1afd114b24786b61fbb8aa94679
Saeed Freg . <raamfarig@gmail.com>
2025-04-25 16:17:55 +0300

    Add files via upload

---
+++ aa.py
@@ -0,0 +1,13 @@
+A = 72 
+B = 88 
+G = 22 
+F = 19 
+L = 11 
+Y = 8 
+R = 99 
+W = 32 
+
+print(A + B) # = The Number ("160")
+print(L - Y) # = The Number ("3")
+print(G * F) # = The Number ("418")
+print(R / W) # = The Number ("3.09375")
---
+++ ss.py
@@ -0,0 +1,11 @@
+aa = 10
+bb = 10
+cc = 30
+dd = 40
+
+if aa == bb:
+    print("Sa3ed Freg")
+if bb < cc:
+    print("Smaller")
+if dd >= aa:
+    print("Bigger")
